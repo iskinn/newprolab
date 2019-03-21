bab4e473c125d7f71ff4e70ee8f2fe06e8e26679
root <root@node1.europe-west1-b.c.apt-retina-232921.internal>
2019-03-21 22:13:26 +0000

    Fix airflow Dag. Add bashoperator

--- lab1/airflow/elasticsearch_to_clickhouse.py
+++ lab1/airflow/elasticsearch_to_clickhouse.py
@@ -6,6 +6,7 @@ import pytz
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash_operator import BashOperator
 
 default_args = {
     'owner': 'airflow',
